8f0a62a7deb2c94f05cc50b7da2b884a89aedc0b
MateBotond1 <mate.botond.2024d@telekimezotur.hu>
2026-09-07 14:28:05 +0200

    duplázás

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿
 int szam1 = 5;
 int szam2 = 9;
-if (szam1 < szam2) Console.WriteLine($"A nagyobb: {szam2}");
-else Console.WriteLine(szam1);
+if (szam1 < szam2)
+{
+    szam2 = szam2 * 2
+    Console.WriteLine($"A nagyobb: {szam2}");
+}
+
+else
+{
+    szam1*2
+    Console.WriteLine(szam1);
+}
